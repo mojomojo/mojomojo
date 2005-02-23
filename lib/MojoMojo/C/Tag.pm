014b9955918d50c71ec85fb3e46fcc20158322fd
marcus <marcus>
2005-02-23 21:45:59 +0000

    added Tag controller

---
+++ lib/MojoMojo/C/Tag.pm
@@ -0,0 +1,28 @@
+package MojoMojo::C::Tag;
+
+use strict;
+use base 'Catalyst::Base';
+
+MojoMojo->action(
+
+    '?list' => sub {
+        my ( $self, $c, $tag ) = @_;
+	
+	$c->stash->{template} = 'tag/list.tt';
+
+	return unless $tag;
+	$c->stash->{tag} = $tag;
+	$c->stash->{pages} = MojoMojo::M::Page->search_by_tag($tag);
+	$c->stash->{related} = MojoMojo::M::Tag->related_to($tag)
+    },
+    '?recent' => sub {
+        my ($self,$c,$tag) = @_;
+	$c->stash->{template} = 'tag/recent.tt';
+	return unless $tag;
+	$c->stash->{tag}   = $tag;
+	$c->stash->{pages} = MojoMojo::M::Page->search_by_tag_and_date($tag);
+	$c->stash->{related} = MojoMojo::M::Tag->related_to($tag)
+    }
+);
+    
+1;
