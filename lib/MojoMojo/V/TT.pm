960854139fb679f5d86ce3e0de2ecfc8130d09e1
marcus <marcus>
2005-02-28 23:47:35 +0000

    Update to set view configuration.

--- lib/MojoMojo/V/TT.pm
+++ lib/MojoMojo/V/TT.pm
@@ -2,5 +2,8 @@ package MojoMojo::V::TT;
 
 use strict;
 use base 'Catalyst::View::TT';
+__PACKAGE__->config->{DEBUG}       = 'all';
+__PACKAGE__->config->{PRE_CHOMP}   = 2;
+__PACKAGE__->config->{POST_CHOMP}  = 2;
 
 1;
