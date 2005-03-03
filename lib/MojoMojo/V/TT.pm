969d205d02f25f3bbe1a59bd44ff32bc7784058b
marcus <marcus>
2005-03-03 16:38:15 +0000

    prepared support for debug flags.

--- lib/MojoMojo/V/TT.pm
+++ lib/MojoMojo/V/TT.pm
@@ -2,7 +2,10 @@ package MojoMojo::V::TT;
 
 use strict;
 use base 'Catalyst::View::TT';
-__PACKAGE__->config->{DEBUG}       = 'all';
+use Template::Constants qw( :debug );
+
+
+#__PACKAGE__->config->{DEBUG}       = DEBUG_UNDEF;
 __PACKAGE__->config->{PRE_CHOMP}   = 2;
 __PACKAGE__->config->{POST_CHOMP}  = 2;
 
