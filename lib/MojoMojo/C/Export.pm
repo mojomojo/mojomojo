5990d902de9ad34691165d1c63acb74feb500636
marcus <marcus>
2005-03-03 21:58:32 +0000

    export working.

--- lib/MojoMojo/C/Export.pm
+++ lib/MojoMojo/C/Export.pm
@@ -6,15 +6,32 @@ use Time::Piece;
 my $model='MojoMojo::M::Core::Page';
 
 MojoMojo->action (
+    '.export' => sub {
+        my ( $self, $c ) = @_;
+				$c->stash->{template} ='export.tt'
+
+		},
     '.export.zip' => sub {
         my ( $self, $c ) = @_;
-
+        my @pages=$model->retrieve_all_sorted_by("node");
+        my $archive=Archive::Zip->new();
+        my $prefix=$c->fixw($c->pref('name')).
+	      "-export-".localtime->ymd('-').'-'.localtime->hms('-');
+        $archive->addDirectory("$prefix/");
+        for my $page (@pages) {
+		      $archive->addString($page->content, $prefix."/".$page->node);
+				}
+    my $fh=IO::Scalar->new(\$c->res->{output});
+    $archive->writeToFileHandle($fh);
+    $c->res->headers->header("Content-Type" => 'archive/zip');
+    $c->res->headers->header("Content-Disposition" =>
+       "attachment; filename=$prefix.zip");
 		},
 		'.html.zip' => sub {
         my ( $self, $c ) = @_;
         my @pages=$model->retrieve_all_sorted_by("node");
         my $archive=Archive::Zip->new();
-        my $prefix=$c->fixw($c->pref('application_name')).
+        my $prefix=$c->fixw($c->pref('name')).
 	      "-html-".localtime->ymd('-').'-'.localtime->hms('-');
         $archive->addDirectory("$prefix/");
 				my $home=$c->pref("home_node");
