8b84dbd0a8bf6660fd7261a27c5d7ac10eae4956
marcus <marcus>
2005-03-03 16:36:01 +0000

    removed extra class.

--- lib/MojoMojo/Formatter/Include.pm
+++ lib/MojoMojo/Formatter/Include.pm
@@ -23,27 +23,6 @@ sub include {
     return "Could not retrieve $url .\n";
 }
 
-package MojoMojo::Formatter::Pod::Simple::HTML;
-
-use base 'Pod::Simple::HTML';
-sub new {
-	my ($class,$base)=@_;
-	my $self= $class->SUPER::new;
-	$self->{_base}=$base;
-	return $self;
-}
-
-sub do_link {
-    my ($self,$token) = @_;
-    my $base="/mitiki";
-    my $link = $token->attr('to');
-    return super unless $link =~ /^$WORD+$/;
-    my $section = $token->attr('section');
-    $section = "#$section"
-      if defined $section and length $section;
-    $self->{base} . "/page/view/$link$section";
-}
-
 
 
 1;
