85e9f2901ab0defbe2355837fc9496483afc7ab2
marcus <marcus>
2005-05-08 21:31:02 +0000

    fix smileys.

--- lib/MojoMojo/Formatter/Emote.pm
+++ lib/MojoMojo/Formatter/Emote.pm
@@ -6,7 +6,7 @@ sub format_content_order { 95 }
 sub format_content {
     my ($self,$content,$base)=@_;
     my $emoticon = Text::Emoticon::MSN->new(
-      imgbase => "$base.static/emote");
+      imgbase => "$base/.static/emote");
     # Let textile handle the rest
     $$content= $emoticon->filter( $$content );
 }
