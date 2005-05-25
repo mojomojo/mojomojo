8a16951640ac47f6e4842fbb98e55dc35c7fe5bf
marcus <marcus>
2005-05-25 13:10:35 +0000

    made amazon plugin more generic fixed better tagging interface

--- lib/MojoMojo/Formatter/Amazon.pm
+++ lib/MojoMojo/Formatter/Amazon.pm
@@ -7,7 +7,6 @@ sub format_content {
     my ($self,$content,$base)=@_;
     my @lines=split /\n/,$$content;
     my $pod;$$content="";
-    warn "Running amazon formatter";
     foreach my $line (@lines) {
       if ($line =~ m/^\=amazon\s+(\w+)\s*$/) { 
          $$content.=$self->blurb($1);
@@ -15,27 +14,39 @@ sub format_content {
         $$content .=$line."\n";
       }
     }
-    warn "content is now ".$$content;
 
 }
 
 sub blurb {
   my ($self,$id)=@_;
-  warn "Formatting $id";
   my $amazon=Net::Amazon->new(token=>'D13HRR2OQKD1Y5');
   my $response=$amazon->search(asin=>$id);
   return "Unable to connect to amazon." unless $response->is_success;
   my ($property)=$response->properties;
+  return "No property object" unless $property;
+  my $method=ref $property; 
+  $method =~ s/.*:://;
   return "<div class=\"amazon\">!<".$property->ImageUrlSmall.
   '!:http://www.amazon.com/exec/obidos/ASIN/'.$id."/feed-20\n\n".
   "h1. ".$property->ProductName."\n\n".
   '"buy at amazon for '.$property->OurPrice.'":'.
-  'http://www.amazon.com/exec/obidos/ASIN/'.$id."/feed-20".
-  ( ref $property eq 'Net::Amazon::Property::DVD' ?
-  "-- ??".join(',',$property->directors).'?? ('.$property->year .')'
-  :
-  " -- ??".join(',',$property->authors).'?? ('.$property->year .")\n\n</div>");
+  'http://www.amazon.com/exec/obidos/ASIN/'.$id."/feed-20\n\n".
+  ($method && $self->can($method) && $self->$method($property));
+}
 
+sub DVD {
+  my ($self,$property) = @_;
+  return "-- ??".join(',',$property->directors).'?? ('.$property->year .')';
+}
+
+sub Book {
+  my ($self,$property) = @_;
+  return " -- ??".join(',',$property->authors).'?? ('.$property->year .")\n\n</div>";
+}
+
+sub Music {
+  my ($self,$property) = @_;
+  return " -- ??".join(',',$property->artists).'?? ('.$property->year .")\n\n</div>";
 }
 
 1;
