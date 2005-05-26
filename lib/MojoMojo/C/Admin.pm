6574a9e37cebd65edf021aa274a8108b27fd7730
marcus <marcus>
2005-05-26 15:19:38 +0000

    oops, some missing files. *blush*

---
+++ lib/MojoMojo/C/Admin.pm
@@ -0,0 +1,80 @@
+package MojoMojo::C::Admin;
+
+use strict;
+use base 'Catalyst::Base';
+
+=head1 NAME
+
+MojoMojo::C::Admin - Catalyst component
+
+=head1 DESCRIPTION
+
+Catalyst admin controller
+
+=head1 METHODS
+
+=over 4
+
+=item auto
+
+=cut
+
+sub auto : Private {
+    my ( $self, $c ) = @_;
+    # FIXME - need to identify administrators
+    my $user = $c->req->{user};
+    my $admins = $c->pref('admins');
+    unless ( $user && $admins =~m/\b$user\b/ ) {
+        $c->stash->{message}='sorry bubba, gotta be admin';
+        $c->stash->{template}='message.tt';
+        $c->log->info('admins are '.$admins);
+        return 0;
+    }
+    $c->log->info('user is '.$user);
+    $admins =~s/$user//g;
+    $c->stash->{admins} = $admins;
+    return 1;
+}
+
+sub pref : Path('/.prefs') {
+    my ( $self, $c ) = @_;
+    $c->stash->{template}='prefs.tt';
+}
+
+sub pref_name : Path('/.prefs/name') {
+    my ( $self, $c ) = @_;
+    $c->form(required=>'name');
+    $c->pref('name',$c->form->valid('name'))
+      unless ($c->form->has_missing ||$c->form->has_invalid); 
+    $c->res->body($c->pref('name'));
+}
+
+sub pref_admins : Path('/.prefs/admins') {
+    my ( $self, $c ) = @_;
+    $c->form(required=>'admins');
+    unless ($c->form->has_missing ||$c->form->has_invalid) {
+      my @users =  split(m/\s+/,$c->form->valid('admins'));
+      foreach my $user ( @users ) {
+        return $c->res->body('Not valid: '.$user)
+          unless (MojoMojo::M::Core::Person->get_user($user)); 
+      }
+      $c->res->body('Updated');
+      $c->pref('admins',join(' ',@users,$c->req->{user}));
+  }
+}
+
+=back
+
+
+=head1 AUTHOR
+
+Marcus Ramberg <mramberg@cpan.org>
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify 
+it under the same terms as perl itself.
+
+=cut
+
+1;
