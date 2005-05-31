e748639d6547784ce036c46253cf1219f7323b6f
marcus <marcus>
2005-05-31 17:35:31 +0000

    added 'small' option to just show a book.

--- lib/MojoMojo/Formatter/Amazon.pm
+++ lib/MojoMojo/Formatter/Amazon.pm
@@ -8,8 +8,20 @@ sub format_content {
     my @lines=split /\n/,$$content;
     my $pod;$$content="";
     foreach my $line (@lines) {
-      if ($line =~ m{^\=http\S+amazon\S+/(?:-|ASIN)/([^/]+)/}) { 
-            $$content.=$self->blurb($1);
+      if ($line =~ m{^\=http\S+amazon\S+/(?:-|ASIN)/([^/]+)/(?:.+\s+(\w+))?}) { 
+          warn "line is $line";
+          warn "action is".$2;
+          my $item=$self->get($1);
+          unless (ref($item)) {
+              $$content.=$item."\n";
+              next;
+          }
+          if ($2) {
+            next unless $self->can($2);
+            $$content.=$self->$2($item);
+          } else {
+            $$content.=$self->blurb($item);
+          }
       } else {
         $$content .=$line."\n";
       }
@@ -17,13 +29,25 @@ sub format_content {
 
 }
 
-sub blurb {
+sub get {
   my ($self,$id)=@_;
   my $amazon=Net::Amazon->new(token=>'D13HRR2OQKD1Y5');
   my $response=$amazon->search(asin=>$id);
   return "Unable to connect to amazon." unless $response->is_success;
-  my ($property)=$response->properties;
+  ($property)=$response->properties;
   return "No property object" unless $property;
+  return $property;
+}
+
+
+sub small {
+  my ($self,$property)=@_;
+  return "!".$property->ImageUrlMedium.
+  '!:http://www.amazon.com/exec/obidos/ASIN/'.$id."/feed-20\n";
+}
+
+sub blurb {
+  my ($self,$property)=@_;
   my $method=ref $property; 
   $method =~ s/.*:://;
   return "<div class=\"amazon\">!<".$property->ImageUrlSmall.
