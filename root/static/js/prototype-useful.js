d7397619b01dc55fc3c453882ce4d0d66a55c6a1
marcus <marcus>
2005-06-17 13:11:10 +0000

    restructured scripts, abstracted out to prototype-useful.

---
+++ root/static/js/prototype-useful.js
@@ -0,0 +1,45 @@
+/* prototype-useful.js - Some useful additions for the prototype library */
+/* Some code from pageplay.js by Peter Cooper */
+/* By Marcus Ramberg */
+
+document.registerAction = function(element,action,functionName) {
+    element=$(element);
+    if (element) {
+        try { 
+            element.addEventListener(action, functionName, false);
+        } catch(e) { alert(e); }
+        try { 
+            element.attachEvent('on'+action, functionName, false);
+        } catch(e) { 
+          element['on'+action]=functionName;
+        }
+    }
+}
+
+
+Form.Element.SmartObserver = Class.create();
+Form.Element.SmartObserver.prototype = {
+    initialize: function(element,delay,callback) {
+        this.delay = delay;
+        this.element = $(element);
+        this.callback = callback;
+        this.lastValue = this.getValue();
+
+        document.registerAction(this.element, 'keyup', this.registerCallback.bind(this));
+    },
+    registerCallback: function() {
+        if (this.timer) clearTimeout(this.timer);
+        this.timer = setTimeout(this.onTimerEvent.bind(this), this.delay*1000);
+    },
+   onTimerEvent: function() {
+      var value = this.getValue();
+      if (this.lastValue != value) {
+          this.callback(this.element, value);
+          this.lastValue = value;
+      }
+    },
+  getValue: function() {
+      return Form.Element.getValue(this.element);
+  }
+}
+
--- root/static/js/wiki.js
+++ root/static/js/wiki.js
@@ -4,3 +4,37 @@ function cleanAuthorName(author) {
   }
 }
 
+function toggleInfo() {
+    state=!state;
+    var item=$('hidden_info');
+    if (state) {
+        item.style.display='block'; 
+        item.style.opacity = 0.99999;
+        item.style.filter = "alpha(opacity:"+100+")";
+    } else {
+        new Effect2.Fade('hidden_info',{duration:0.3})
+    }
+}
+
+var diff='';
+function toggleChanges(changeurl) {
+  if (!diff) {
+      diff=xmlHTTPRequest(changeurl);
+      $('diff').innerHTML=diff;
+  }
+  Toggle.display('changes');
+  Toggle.display('current');
+  Toggle.display('show_changes');
+  Toggle.display('hide_changes');
+  /* if ($('changes').style.display == "none") {
+    $('changes').style.display  = "block";
+    $('current').style.display = "none";
+    $('show_changes').style.display  = "none";
+    $('hide_changes').style.display = "inline";
+  } else {
+    $('changes').style.display  = "none";
+    $('current').style.display = "block";
+    $('show_changes').style.display  = "inline";
+    $('hide_changes').style.display = "none";
+  }*/
+}
