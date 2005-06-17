b28f9284d13eba83733b70c33eae55ea88fc6baa
marcus <marcus>
2005-06-17 22:19:21 +0000

    turn off timing data in HTML (breaks validation)

--- lib/MojoMojo/V/TT.pm
+++ lib/MojoMojo/V/TT.pm
@@ -8,6 +8,7 @@ use Template::Constants qw( :debug );
 #__PACKAGE__->config->{DEBUG}       = DEBUG_UNDEF;
 __PACKAGE__->config->{PRE_CHOMP}   = 2;
 __PACKAGE__->config->{POST_CHOMP}  = 2;
+ __PACKAGE__->config->{CONTEXT} = undef;
 
 1;
 
