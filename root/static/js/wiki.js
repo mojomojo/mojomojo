b47061c80b2928539af88018936e27d2bc8b0cf4
marcus <marcus>
2005-06-27 07:43:16 +0000

    use built-in string replace function. set utf8 form encoding.

--- root/static/js/wiki.js
+++ root/static/js/wiki.js
@@ -1,13 +1,3 @@
-function replaceString(oldS,newS,fullS) {
-// Replaces oldS with newS in the string fullS
-   for (var i=0; i<fullS.length; i++) {
-      if (fullS.substring(i,i+oldS.length) == oldS) {
-         fullS = fullS.substring(0,i)+newS+fullS.substring(i+oldS.length,fullS.length)
-      }
-   }
-   return fullS
-}
-
 function cleanAuthorName(author) {
   if (document.getElementById('authorName').value == "") {
     document.getElementById('authorName').value = author;
