0e92b6b72cbca1ada5ab6144f52997b50d7f3516
marcus <marcus>
2005-07-19 22:33:32 +0000

    no caching of edit page.

--- lib/MojoMojo.pm
+++ lib/MojoMojo.pm
@@ -99,7 +99,12 @@ sub end : Private {
     return 1 if $c->response->status =~ /^3\d\d$/;
     return 1 if $c->response->body;
     if ($c->stash->{page}) {
-      $c->res->header('Last-Modified',$c->stash->{page}->content->pub_date);
+        if ($c->req->action() eq 'edit') {
+            $c->res->header('Cache-Control','no-cache');
+        } else {
+            $c->res->header('Last-Modified',
+                $c->stash->{page}->content->pub_date);
+        }
     }
     unless ( $c->response->content_type ) {
        $c->response->content_type('text/html; charset=utf-8');
