5cbe36fa6fa0b3a4dc12c0e95caa4de3786352ee
marcus <marcus>
2005-07-20 13:30:48 +0000

    add the actual toolbar fix some special chars in ajax (+/;/&)

--- root/static/js/wiki.js
+++ root/static/js/wiki.js
@@ -26,15 +26,82 @@ function toggleChanges(changeurl) {
   Toggle.display('current');
   Toggle.display('show_changes');
   Toggle.display('hide_changes');
-  /* if ($('changes').style.display == "none") {
-    $('changes').style.display  = "block";
-    $('current').style.display = "none";
-    $('show_changes').style.display  = "none";
-    $('hide_changes').style.display = "inline";
-  } else {
-    $('changes').style.display  = "none";
-    $('current').style.display = "block";
-    $('show_changes').style.display  = "inline";
-    $('hide_changes').style.display = "none";
-  }*/
 }
+
+function encodeAjax (str) {
+   str=str.replace(/%/g,'%25');
+   str=str.replace(/&/g,'%26');
+   str=str.replace(/\+/g,'%2b');
+   str=str.replace(/\;/g,'%3b');
+   return str;
+}
+
+// apply tagOpen/tagClose to selection in textarea,
+// use sampleText instead of selection if there is none
+//
+// copied and adapted from wikipedia, who
+// copied and adapted from phpBB
+
+function insertTags(txtarea,tagOpen, tagClose, sampleText) {
+
+  txtarea = $(txtarea);
+  // IE
+  if(document.selection ) {
+    var theSelection = document.selection.createRange().text;
+    if(!theSelection) { theSelection=sampleText;}
+    txtarea.focus();
+    if(theSelection.charAt(theSelection.length - 1) == " "){// exclude ending space char, if any
+      theSelection = theSelection.substring(0, theSelection.length - 1);
+      document.selection.createRange().text = tagOpen + theSelection + tagClose + " ";
+    } else {
+      document.selection.createRange().text = tagOpen + theSelection + tagClose;
+    }
+
+  // DOM
+  } else if(txtarea.selectionStart || txtarea.selectionStart == '0') {
+    var startPos = txtarea.selectionStart;
+    var endPos = txtarea.selectionEnd;
+    var scrollTop=txtarea.scrollTop;
+    var myText = (txtarea.value).substring(startPos, endPos);
+    if(!myText) { myText=sampleText;}
+    if(myText.charAt(myText.length - 1) == " "){ // exclude ending space char, if any
+      subst = tagOpen + myText.substring(0, (myText.length - 1)) + tagClose + " ";
+    } else {
+      subst = tagOpen + myText + tagClose;
+    }
+    txtarea.value = txtarea.value.substring(0, startPos) + subst +
+      txtarea.value.substring(endPos, txtarea.value.length);
+    txtarea.focus();
+
+    var cPos=startPos+(tagOpen.length+myText.length+tagClose.length);
+    txtarea.selectionStart=cPos;
+    txtarea.selectionEnd=cPos;
+    txtarea.scrollTop=scrollTop;
+
+  // All others
+  } else {
+    var copy_alertText=alertText;
+    var re1=new RegExp("\\$1","g");
+    var re2=new RegExp("\\$2","g");
+    copy_alertText=copy_alertText.replace(re1,sampleText);
+    copy_alertText=copy_alertText.replace(re2,tagOpen+sampleText+tagClose);
+    var text;
+    if (sampleText) {
+      text=prompt(copy_alertText);
+    } else {
+      text="";
+    }
+    if(!text) { text=sampleText;}
+    text=tagOpen+text+tagClose;
+    document.infoform.infobox.value=text;
+    // in Safari this causes scrolling
+    if(!is_safari) {
+      txtarea.focus();
+    }
+    noOverwrite=true;
+  }
+  // reposition cursor if possible
+  if (txtarea.createTextRange) txtarea.caretPos = document.selection.createRange().duplicate();
+}
+
+
