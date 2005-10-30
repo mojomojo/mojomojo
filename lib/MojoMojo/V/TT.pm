003fdc4be332006cb713d5089b965fe6f095cc99
marcus <marcus>
2005-10-30 17:54:15 +0000

    make sure timer is off;

--- lib/MojoMojo/V/TT.pm
+++ lib/MojoMojo/V/TT.pm
@@ -8,7 +8,8 @@ use Template::Constants qw( :debug );
 #__PACKAGE__->config->{DEBUG}       = DEBUG_UNDEF;
 __PACKAGE__->config->{PRE_CHOMP}   = 2;
 __PACKAGE__->config->{POST_CHOMP}  = 2;
- __PACKAGE__->config->{CONTEXT} = undef;
+__PACKAGE__->config->{CONTEXT} = undef;
+__PACKAGE__->config->{TIMER} =0;
 
 1;
 
