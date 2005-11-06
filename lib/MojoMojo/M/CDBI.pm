9c29253e0d933b1d7617ade9891d4696c0f73db1
marcus <marcus>
2005-11-06 13:47:03 +0000

    abandon ::sweet hackery for left_base_classes

--- lib/MojoMojo/M/CDBI.pm
+++ lib/MojoMojo/M/CDBI.pm
@@ -3,7 +3,6 @@ package MojoMojo::M::CDBI;
 use strict;
 use base 'Catalyst::Model::CDBI';
 
-use Catalyst::Model::CDBI::Sweet;
 
 die "No DSN defined" unless MojoMojo->config->{dsn};
 
@@ -25,6 +24,7 @@ CDBI::Loader to set the classes from sql.
 __PACKAGE__->config(
     dsn                => MojoMojo->config->{dsn},
     namespace          => 'MojoMojo::M::Core',
+    left_base_classes  => qw/Class::DBI::Sweet/,
     additional_classes => [
         qw/Class::DBI::FromForm Class::DBI::Sweet::Topping Class::DBI::AsXML/
     ],
@@ -39,12 +39,7 @@ Overridden to make classes ::Sweet subclasses.
 sub new {
      my $class = shift;
      my $self = $class->NEXT::new(@_);
-     foreach my $subclass ( $self->loader->classes ) {
-         no strict 'refs';
-         unshift @{ $subclass . '::ISA' }, 'Catalyst::Model::CDBI::Sweet';
-     }
-     return $self;
-      # uncomment this to get dbh traces
+     # uncomment this to get dbh traces
 #     ($self->loader->classes)[0]->db_Main()->trace(1);
 }
 
