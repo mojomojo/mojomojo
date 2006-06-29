4ecb580bdb8ca8d7e78b0db20b2c66c82140a747
marcus <marcus>
2006-06-29 02:13:59 +0000

    oops, broke login.

--- lib/MojoMojo/Controller/Root.pm
+++ lib/MojoMojo/Controller/Root.pm
@@ -17,9 +17,7 @@ sub begin : Private {
 	    ( $path_pages, $proto_pages );
         $c->stash->{page} = $path_pages->[ @$path_pages - 1 ];
 	# FIXME: new user stuff.
-#        $c->req->{user_id} && do {
-            $c->stash->{user} = $c->user->obj();
-#        };
+         $c->stash->{user} = $c->user->obj() if $c->user_exists;
     }
 }
 
