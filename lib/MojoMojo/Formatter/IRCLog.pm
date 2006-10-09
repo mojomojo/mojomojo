06a5e1c763f97669a9ca9de03c728f7c18d9ae4c
martijn <martijn>
2006-10-09 19:27:36 +0000

    r2127@debian:  martijn | 2006-10-09 21:24:02 +0200  Add a MojoMojo::Formatter for IRC logs. now we can easily paste in  irc logs, and they will look nice on the wiki. yay!

---
+++ lib/MojoMojo/Formatter/IRCLog.pm
@@ -0,0 +1,112 @@
+package MojoMojo::Formatter::IRCLog;
+
+=head1 NAME
+
+MojoMojo::Formatter::IRCLog - format part of content as an IRC log
+
+=head1 DESCRIPTION
+
+This formatter will format content between two =irc blocks as 
+an IRC log
+
+=head1 METHODS
+
+=over 4
+
+=item format_content_order
+
+Format order can be 1-99. The IRC log formatter runs on 14, so
+just before the Textile formatter.
+
+=cut
+
+sub format_content_order { 14 }
+
+
+
+=item format_content
+
+calls the formatter. Takes a ref to the content as well as the
+context object.
+
+=cut
+
+sub format_content {
+    my ($class, $content) = @_;
+    my ($in_log, %nicks, $longline, @newlines);
+
+    my @lines = split(/\n/, $$content);
+    $$content="";
+
+    foreach my $line (@lines) {
+	if ($in_log) {
+	    if ($line =~ /^=irc/) {
+		$in_log = 0;
+		if ($longline) {
+		    $longline .= "</dd>";
+		    push (@newlines, $longline);
+		}
+		push @newlines, $line;
+	    } elsif ($line =~ /^[\d:]*\s*<[+\%\@ ]?(.*?)>\s*(.*)/) {
+		if ($longline) {
+		    $longline .= "</dd>";
+		    push (@newlines, $longline);
+		}
+		$nicks{$1} = 1;
+		$longline = "<dt>$1</dt>\n<dd>$2";
+	    } else {
+		$line =~ s/^\s*/ /;
+		$longline .= $line;
+	    }
+	} else {
+	    if ($line =~ m/^=irc\s*$/) { 
+		push @newlines, $line;
+		$in_log = 1;
+	    } else {
+		push (@newlines, $line);
+	    }
+	}
+    }
+    foreach my $line (@newlines) {
+	if ($in_log) {
+	    if ($line =~ /^=irc/) {
+		$in_log = 0;
+		# end the dl and the section not handled by textile
+		$$content .= "</dl>\n==\n";
+	    } else {
+		my $count = 0;;
+		foreach my $nick (keys %nicks) {
+		    $count += ($line =~ s/$nick/[[$nick]]/g);
+		}
+		    $$content .= "$line\n";
+	    }
+	} else {
+	    if ($line =~ /^=irc/) {
+		$in_log = 1;
+		# start a definition list in a section not handled by
+		# textile
+		$$content .= "==\n<dl>\n";
+	    } else {
+		$$content .= "$line\n";
+	    }
+	}
+    }
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<MojoMojo> and L<Module::Pluggable::Ordered>,
+
+=head1 AUTHORS
+
+Martijn van Beers <martijn@eekeek.org>
+
+=head1 LICENSE
+
+This module is licensed under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/formatter_irclog.t
@@ -0,0 +1,35 @@
+use Test::More qw(no_plan);
+
+use MojoMojo::Formatter::IRCLog;
+
+
+
+my $content;
+
+my $ib = "\n=irc\n";
+my $ie = "\n=irc\n";
+my $ob = "\n==\n<dl>\n";
+my $oe = "</dl>\n==\n";
+$content = "${ib}<nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "basic");
+$content = "${ib}12:00 <nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "with timestamp");
+$content = "${ib}12:00 < nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "with space");
+$content = "${ib}12:00 <\@nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "op");
+$content = "${ib}12:00 <%nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "half-op");
+$content = "${ib}12:00 <+nick> text$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text</dd>\n$oe", "voice");
+$content = "${ib}12:00 <+nick> text\nmore$ie";
+MojoMojo::Formatter::IRCLog->format_content(\$content);
+is($content, "$ob<dt>[[nick]]</dt>\n<dd>text more</dd>\n$oe", "multi-line");
+
+
