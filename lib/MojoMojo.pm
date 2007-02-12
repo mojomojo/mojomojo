d6315837a3e6888f02aa0e36d647d550b3a79adc
marcus <marcus>
2007-02-12 10:32:11 +0000

    remove stacktrace

--- lib/MojoMojo.pm
+++ lib/MojoMojo.pm
@@ -12,7 +12,6 @@ use Catalyst qw/-Debug              Authentication
 		Singleton           Session::State::Cookie
 		Static::Simple	    SubRequest	    
 		UploadProgress	    Unicode 
-		StackTrace
 		Authentication::Store::DBIC 
 		ConfigLoader	    Authentication::Credential::Password 
 		/;
