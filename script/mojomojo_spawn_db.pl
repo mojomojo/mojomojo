ca7f40ed4b8deb6b8abd980a30dad02fe90fa55f
jrockway <jrockway>
2007-06-30 00:51:29 +0000

    fix deploy script (a bit)

--- script/mojomojo_spawn_db.pl
+++ script/mojomojo_spawn_db.pl
@@ -1,93 +1,43 @@
-#!/usr/bin/perl
-# kt_timeline_spawndb.pl 
-# Copyright (c) 2006 Jonathan Rockway <jrockway@cpan.org> 
-# Copied from older mojomojo_spawndb.pl by K. J. Cheetham <jamie@shadowcatsystems.co.uk> 
-# Perl license.
+#!/usr/bin/env perl
+# Copyright (c) 2007 Jonathan Rockway <jrockway@cpan.org>
 
-use warnings;
 use strict;
-use DateTime;
-use Getopt::Long;
-use Pod::Usage;
-use Path::Class;
-use Config::Any;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
+use warnings;
+use FindBin '$Bin';
+use lib "$Bin/../lib";
 use MojoMojo::Schema;
-use YAML;
 
-my @databases      = [ qw/ MySQL SQLite PostgreSQL Oracle XML YAML / ];
-my $help           = 0;
-my $bin            = dir($FindBin::Bin);
-my $config         = YAML::LoadFile(file($bin->parent, 'mojomojo.yml'));
-my $deploy         = 0;
-my $create_ddl_dir = 0;
-my $attrs          = {add_drop_table => 1, no_comments => 1};
-my $type           = '';
+my $dsn = shift @ARGV;
+die "please pass the 'DBI:...' connect string as the first argument"
+  unless $dsn;
 
+my $schema = MojoMojo::Schema->connect($dsn) or 
+  die "Failed to connect to database";
 
-my ($user, $pass, $dsn);
-GetOptions('help|?'         => \$help,
-	   'dsn=s'          => \$dsn,
-	   'user=s'         => \$user,
-	   'pass=s'         => \$pass,
-	   'deploy'         => \$deploy,
-	   'create_ddl_dir' => \$create_ddl_dir,
-	  );
+$schema->deploy;
+my @people = $schema->populate('Person', [
+                                      [ qw/ active views photo login name email pass timezone born gender occupation industry interests movies music / ],
+                                      [ 1,0,0,'AnonymousCoward','Anonymous Coward','','','',0,'','','','','','' ],
+                                      [ 1,0,0,'admin','Enoch Root','','admin','',0,'','','','','','' ],
+                                     ]);
 
-pod2usage(1) if ($help);
+$schema->populate('Preference', [
+                             [ qw/ prefkey prefvalue / ],
+                             [ 'name','MojoMojo' ],
+                             [ 'admins','admin' ],
+                            ]);
 
-my $config_dsn;
-eval { 
-    ($config_dsn, $user, $pass) = 
-      @{$config->{'Model::DBIC'}->{'connect_info'}};
-};
-if($@){
-    die "Your DSN line in mojomojo.yml doesn't look like a valid DSN."
-}
-$dsn = $config_dsn if(!$dsn);
-die "No valid Data Source Name (DSN).\n" if !$dsn;
+$schema->populate('PageVersion', [
+                              [ qw/page version parent parent_version name name_orig depth
+                                   content_version_first content_version_last creator status created
+                                   release_date remove_date comments/ ],
+                              [ 1,1,undef,undef,'/','/',0,1,1, $people[1]->id,'',0,'','','' ],
+                             ]);
 
-($type) = ($dsn =~ m/:(.+?):/);
-$type = 'MySQL' if $type eq 'mysql';
-
-$dsn =~ s/__HOME__/$FindBin::Bin\/\.\./g;
-
-my $db = MojoMojo::Schema->connect($dsn, $user, $pass, $attrs);
-if ($create_ddl_dir) {
-    print $db->storage->create_ddl_dir($db, @databases, '0.1', 
-				       "$FindBin::Bin/../db/", $attrs);
-}
-else {
-    print "Connecting to $dsn\n";
-    print " as $user\n" if $user;
-    print " with password\n" if $pass;
-    $db->storage->ensure_connected;
-    $db->deploy( $attrs );
-
-    my @people = $db->populate('Person', [
-					  [ qw/ active views photo login name email pass timezone born gender occupation industry interests movies music / ],
-					  [ 1,0,0,'AnonymousCoward','Anonymous Coward','','','',0,'','','','','','' ],
-					  [ 1,0,0,'admin','Enoch Root','','admin','',0,'','','','','','' ],
-					 ]);
-    
-    $db->populate('Preference', [
-				 [ qw/ prefkey prefvalue / ],
-				 [ 'name','MojoMojo' ],
-				 [ 'admins','admin' ],
-				]);
-    
-    $db->populate('PageVersion', [
-				  [ qw/page version parent parent_version name name_orig depth
-				       content_version_first content_version_last creator status created
-				       release_date remove_date comments/ ],
-				  [ 1,1,undef,undef,'/','/',0,1,1, $people[1]->id,'',0,'','','' ],
-				 ]);
-    
-    $db->populate('Content', [
-			      [ qw/ page version creator created body status release_date remove_date type abstract comments 
-				    precompiled / ],
-			      [ 1,1, $people[1]->id, 0,'h1. Welcome to MojoMojo!
+$schema->populate('Content', [
+                          [ qw/ page version creator created body status release_date remove_date type abstract comments 
+                                precompiled / ],
+                          [ 1,1, $people[1]->id, 0,'h1. Welcome to MojoMojo!
 
 This is your front page. To start administrating your wiki, please log in with
 username admin/password admin. At that point you will be able to set up your
@@ -103,45 +53,12 @@ Check out our [[Help]] section.','released','','','','','','' ],
 * Formatter Syntax.
 * Using Tags
 * Attachments & Photos','released','','','','','','' ],
-			     ]);
-    
-    $db->populate('Page', [
-			   [ qw/ id version parent name name_orig depth lft rgt content_version / ],
-			   [ 1,1,undef,'/','/',0,1,4,1 ],
-			   [ 2,1,1,'help','Help',1,2,3,1 ],
-			  ]);
-}
+                         ]);
 
-1;
+$schema->populate('Page', [
+                       [ qw/ id version parent name name_orig depth lft rgt content_version / ],
+                       [ 1,1,undef,'/','/',0,1,4,1 ],
+                       [ 2,1,1,'help','Help',1,2,3,1 ],
+                      ]);
 
-__END__
-
-=head1 NAME
-
-mojomojo_spawn_db - prodcues the sql statements needed to create a MojoMojo
-database
-
-=head1 SYNOPSIS
-
-mojomojo_spawndb.pl [options]
-
- Options:
-   -help              Display this help and exit
-   -create_ddl_dir    Create SQL files for common databases in /db. 
-                      Requires SQL::Translator installed.
- Example:
-    mojomojo_spawndb.pl
-
- See also:
-    perldoc MojoMojo
-
-=head1 SEE ALSO
-
-L<MojoMojo>
-
-=head1 COPYRIGHT
-
-This library is free software. You can redistribute it and/or modify it under
-the same terms as perl itself.
-
-=cut
+  
