d4c0c3d8d5f3a90d023d6b4728297ef117b10980
jayk <jayk>
2008-02-05 18:24:02 +0000

    Correcting PathPermissions to use varchar, and updating makefile with proper cache modules

--- lib/MojoMojo/Schema/PathPermissions.pm
+++ lib/MojoMojo/Schema/PathPermissions.pm
@@ -14,17 +14,17 @@ __PACKAGE__->add_columns(
   "role",
     { data_type => "INTEGER", is_nullable => 0, size => undef },
   "apply_to_subpages",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },
+    { data_type => "VARCHAR", is_nullable => 0, size => 3 },
   "create_allowed",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },
+    { data_type => "VARCHAR", is_nullable => 1, size => 3 },
   "delete_allowed",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },
+    { data_type => "VARCHAR", is_nullable => 1, size => 3 },
   "edit_allowed",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },     
+    { data_type => "VARCHAR", is_nullable => 1, size => 3 },     
   "view_allowed",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },     
+    { data_type => "VARCHAR", is_nullable => 1, size => 3 },     
   "attachment_allowed",
-    { data_type => "ENUM", is_nullable => 0, size => 3 },     
+    { data_type => "VARCHAR", is_nullable => 1, size => 3 },     
 );
 __PACKAGE__->set_primary_key("path", "role", "apply_to_subpages");
 __PACKAGE__->belongs_to("role", "Role", { id => "role" });
