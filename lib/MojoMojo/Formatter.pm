0a8512011cf5870c12a10006234275be3eb97b97
marcus <marcus>
2008-07-25 10:35:13 +0000

    Documented writing your own formatter

--- lib/MojoMojo/Formatter.pm
+++ lib/MojoMojo/Formatter.pm
@@ -6,17 +6,68 @@ sub primary_formatter { 0; }
 
 MojoMojo::Formatter - Base class for all formatters
 
+=head1 SYNOPSIS
+
+    package MojoMojo::Formatter::Simple;
+    
+    use parent qw/MojoMojo::Formatter/;
+    
+    sub format_content_order { 1 } 
+    
+    sub format_content {
+        my ($class,$content,$c)=@_;
+        $$content =~ s/fuck/f**k/g;
+        return $content;
+    }
+    
+
 =head1 DESCRIPTION
 
-Base class for all l<MojoMojo> Formatters.
+This is the class to inherit from if you want to write your own formatter.
+
+
+=head1 WRITING YOUR OWN FORMATTER
+
+See the synopsis for a really simple formatter example. MojoMojo uses 
+L<Module::Pluggable::Ordered> to process all the formatter plugins. Just 
+specify when you want to trigger your formatter by providing a format_content_order
+method which returns a number to specify when you want to run. The  plugin order 
+for the default plugins is currently as follows:
+
+=over 4
+
+=item 1  - L<MojoMojo::Formatter::Redirect> - handles =redirect
+
+=item 6  - L<MojoMojo::Formatter::Include> - handles =http://<url>
+
+=item 7  - L<MojoMojo::Formatter::Scrub> - Removes harmful HTML
+
+=item 14 - L<MojoMojo::Formater::IRCLog> - handles =irc ... =irc blocks
+
+=item 15 - Main formatter (either L<MojoMojo::Formatter::Textile> or L<MojoMojo::Formatter::Markdown>)
+
+=item 10 - L<MojoMojo::Formatter::Pod> - handles =pod ... =pod blocks
+
+=item 30 - L<MojoMojo::Formatter::Wiki> - Handles [[wikiwords]]
+
+=item 91 - L<MojoMojo::Formatter::Comment> Handles =comments , inserts a comment box
+
+=back
+
+Note that if your formatter expects a HTML body, it should run after the
+main formatter
 
 =head1 METHODS
+
+You can also override further methods to your formatter:
 
 =head2 primary_formatter
 
 Primary formatters are those who handle the basic job of translating markup to HTML. 
 In the default distribution there are currently two, Textile and Markdown, with Textile
-being the default setting. You can change this through Prefs.
+being the default setting. You can change this through Prefs. Override this method to
+return 1 to contend for as a primary formatter. Note that primary formatters should 
+run at 15.
 
 
 =head1 SEE ALSO
