081866a2c4a01677ec135a923c2ed365d545c24a
Marcus Ramberg <marcus@nordaaker.com>
2008-11-14 17:32:24 +0100

    Prepare for release

--- lib/MojoMojo.pm
+++ lib/MojoMojo.pm
@@ -25,7 +25,7 @@ use Module::Pluggable::Ordered
     except      => qr/^MojoMojo::Plugin::/,
     require     => 1;
 
-our $VERSION = '0.999021';
+our $VERSION = '0.999022';
 
 MojoMojo->config->{authentication}{dbic} = {
     user_class     => 'DBIC::Person',
