8690f6fb2f4cbd6c2d2408766c3d152f48f46586
Dan Dascalescu <bigbang7@gmail.com>
2008-11-22 08:17:04 +0000

    Added TOC parameters (min-max header level) and full XHTML compliance

--- lib/MojoMojo/Formatter/TOC.pm
+++ lib/MojoMojo/Formatter/TOC.pm
@@ -30,30 +30,61 @@ sub format_content_order { 95 }
 
 =item format_content
 
-calls the formatter. Takes a ref to the content as well as the
-context object.
+Calls the formatter. Takes a ref to the content as well as the context object.
+The format for the toc plugin invocation is:
+
+  =toc M-     # start from Header level M
+  =toc -N     # stop at Header level N
+  =toc M-N    # process only header levels M..N
+
+where M is the minimum heading level to include in the TOC, and N is the
+maximum level (depth). For example, suppose you only have one H1 on the page
+so it doesn't make sense to add it to the TOC; also, assume you and don't want
+to include any headers smaller than H3. The =toc markup to achieve that would be:
+
+  =toc 2-3
+  
+Defaults to 1-6.
 
 =cut
 
 sub format_content {
     my ( $class, $content ) = @_;
 
-    # replace the =toc markup tag if it's on a line of its own (<p>), or not (followed by <br />)
-    if ($$content =~ s{<p>=toc</p>|(?<=>)?=toc(?=<br)}{<!--mojomojoTOCwillgohere-->}g) {
+    my $toc_params_RE = qr/\s+ (\d+)? \s* - \s* (\d+)?/x;
+    while (
+        # replace the =toc markup tag if it's on a line of its own (<p>), or not (followed by <br />)
+        $$content =~ s{
+            (<p>)=toc(?:$toc_params_RE)? \s* </p>
+          | (?<=>)?=toc(?:$toc_params_RE)? \s* (?=<br)
+        }{<!--mojomojoTOCwillgohere-->}x) {
+        my ($toc_h_min, $toc_h_max);
+        # FIXME: Perl 5.10 has a regexp branch reset operator which simplifies this
+        if ($1) {
+            # matched the <p> branch
+            $toc_h_min = $2 || 1;
+            $toc_h_max = $4 || 12;
+        } else {
+            # matched the <br branch
+            $toc_h_min = $4 || 1;
+            $toc_h_max = $5 || 12;  # 6 is the HTML max, but hey, knock yourself out
+        }
+        
+        my %toc_entry, my %toc_end;
+        for ($toc_h_min..$toc_h_max) {
+            $toc_entry{"h$_"} = $_;
+            $toc_end{"h$_"} = "/h$_";
+        }
+        
         # workaround for http://rt.cpan.org/Public/Bug/Display.html?id=40983
         local *STDOUT;
         my $output;
         open STDOUT, '>:utf8', \$output or die "Can't open STDOUT: $!";
         
         my $toc = new HTML::GenToc(
-            toc_entry => {
-                h1 => 1, h2 => 2, h3 => 3,
-                h4 => 4, h5 => 5, h6 => 6
-            },
-            toc_end => {
-                h1 => '/h1',  h2 => '/h2',  h3 => '/h3',
-                h4 => '/h4',  h5 => '/h5',  h6 => '/h6',
-            },
+            toc_entry => \%toc_entry,
+            toc_end => \%toc_end,
+            toclabel => '',  # no default "Table of Contents" header. The user can add it if needed
             toc_tag => '!--mojomojoTOCwillgohere--',
             toc_tag_replace => 1,
             use_id => 1  # to avoid the different style applied by default to <a name> elements
@@ -66,6 +97,7 @@ sub format_content {
             output => '',
         );
         $$content = $output;
+        return 1;
     }
 }
 
@@ -105,16 +137,15 @@ anchor names.
         # generate a SEO-friendly anchor right from the token content
         # The allowed character set is limited first by the URI specification for fragments, http://tools.ietf.org/html/rfc3986#section-2: characters
         # then by the limitations of the values of 'id' and 'name' attributes: http://www.w3.org/TR/REC-html40/types.html#type-name
-        # Of all ASCII punctuation, !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~, only -_.: are allowed in id values
-        # high-ASCII characters (e.g. Chinese characters) are allowed (test "<h1 id="行政区域">header</h1>" at http://validator.w3.org/#validate_by_input+with_options), except for smart quotes (!), see http://www.w3.org/Search/Mail/Public/search?type-index=www-validator&index-type=t&keywords=[VE][122]+smart+quotes&search=Search+Mail+Archives
+        # Eventually, the only punctuation allowed in id values is [_.:-]
+        # Unicode characters with code points > 0x7E (e.g. Chinese characters) are allowed (test "<h1 id="行政区域">header</h1>" at http://validator.w3.org/#validate_by_input+with_options), except for smart quotes (!), see http://www.w3.org/Search/Mail/Public/search?type-index=www-validator&index-type=t&keywords=[VE][122]+smart+quotes&search=Search+Mail+Archives
         # However, that contradicts the HTML 4.01 spec: "Anchor names should be restricted to ASCII characters." - http://www.w3.org/TR/REC-html40/struct/links.html#h-12.2.1
+        # ...and the [A-Za-z] class of letters mentioned at http://www.w3.org/TR/REC-html40/types.html#type-name
         # Finally, note that pod2html fails miserably to generate XHTML-compliant anchor links. See http://validator.w3.org/check?uri=http%3A%2F%2Fsearch.cpan.org%2Fdist%2FCatalyst-Runtime%2Flib%2FCatalyst%2FRequest.pm&charset=(detect+automatically)&doctype=XHTML+1.0+Transitional&group=0&user-agent=W3C_Validator%2F1.606
         $name =~ s/\s/_/g;
         decode_entities($name);  # we need to replace [#&;] only when they are NOT part of an HTML entity. decode_entities saves us from crafting a nasty regexp
-        use utf8;  # necessary for replacing “”, even in perl 5.10; see http://www.perlmonks.org/?node=Unicode%2C%20regex%27s%2C%20encodings%2C%20and%20all%20that%20(Perl%205.6%20and%205.8)
-        $name =~ s/([!"#\$%&'()*+,\/;<=>?@\[\\\]\^`{|}~“”])/'.'.sprintf('%02X', ord($1))/eg;  # MediaWiki also uses the period, see http://en.wikipedia.org/wiki/Hierarchies#Ethics.2C_behavioral_psychology.2C_philosophies_of_identity
-        $name = 'L'.$name if $name =~ /\A[^A-Za-z]/; # "ID and NAME tokens must begin with a letter ([A-Za-z])"
-        
+        $name =~ s/([^\w_.:-])/'.'.sprintf('%02X', ord($1))/eg;  # MediaWiki also uses the period, see http://en.wikipedia.org/wiki/Hierarchies#Ethics.2C_behavioral_psychology.2C_philosophies_of_identity
+        $name = 'L'.$name if $name =~ /\A\W/; # "ID and NAME tokens must begin with a letter ([A-Za-z])"
     }
     $name = 'id' if $name eq '';
 
