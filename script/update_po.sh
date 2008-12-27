dd0c14b42150679a3cacd9dea28d71f416a20937
Johannes Plunien <plu@pqpq.de>
2008-12-27 12:05:49 +0100

    Added new language to update_po.sh: DE

--- script/update_po.sh
+++ script/update_po.sh
@@ -2,7 +2,7 @@
 
 #find lib root script -type f -not -path '*.svn*' \( -name '*.pm' -or -name '*.tt' -or -name '*.js' -or -name '*.yml' \) > filelist.tmp
 
-    for i in 'en' 'no' ; do
+    for i in 'en' 'no' 'de' ; do
     echo "lang: $i"
 	  perl /usr/bin/xgettext.pl -D lib -D root -o lib/MojoMojo/I18N/$i.po
 done;
