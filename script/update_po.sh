67169bb4d2db9518ec7955dd476ba191b11baa31
Robert Litwiniec <linio@wonder.pl>
2009-02-26 19:59:23 +0100

    added currently supported languages in loop, use 'which' instead hardcoded path

--- script/update_po.sh
+++ script/update_po.sh
@@ -2,8 +2,8 @@
 
 #find lib root script -type f -not -path '*.svn*' \( -name '*.pm' -or -name '*.tt' -or -name '*.js' -or -name '*.yml' \) > filelist.tmp
 
-    for i in 'en' 'no' 'de' 'fr' 'ja' ; do
+    for i in 'en' 'de' 'fr' 'ja' 'no' 'ca' 'es' 'pl' ; do
     echo "lang: $i"
-	  perl -Ilib /usr/bin/xgettext.pl -D lib -D root -o lib/MojoMojo/I18N/$i.po
+	  perl -Ilib `which xgettext.pl` -D lib -D root -o lib/MojoMojo/I18N/$i.po
 done;
 
