7a20c0085a9592f1a0d7843bb4ea3052ef186a7f
Dan Dascalescu <ddascaNOSPAMlescu@gmail.com>
2009-03-02 00:45:44 +0000

    Updated installation to link to the Wiki and mention prerequisites.

--- script/update_po.sh
+++ script/update_po.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-#find lib root script -type f -not -path '*.svn*' \( -name '*.pm' -or -name '*.tt' -or -name '*.js' -or -name '*.yml' \) > filelist.tmp
-
-    for i in 'en' 'de' 'fr' 'ja' 'no' 'ca' 'es' 'pl' ; do
+for i in 'en' 'de' 'fr' 'ja' 'no' 'ca' 'es' 'pl' ; do
     echo "lang: $i"
-	  perl -Ilib `which xgettext.pl` -D lib -D root -o lib/MojoMojo/I18N/$i.po
+	perl -Ilib `which xgettext.pl` -D lib -D root -o lib/MojoMojo/I18N/$i.po
 done;
-
