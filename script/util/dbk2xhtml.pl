2e2242dacadf61677ee42b49966a7f6824e6993b
dab () <dab@catapulse.org>
2009-04-03 00:08:17 +0200

    Add Author and license

--- script/util/dbk2xhtml.pl
+++ script/util/dbk2xhtml.pl
@@ -11,9 +11,7 @@ use File::Slurp;
 
 my $content = read_file( shift ) ;
 
-
 print MojoMojo::Formatter::DocBook->to_dbk( $content );
-
 
 
 __END__
@@ -21,5 +19,13 @@ __END__
 =head1 Usage
 
 dbk2xhtml.pl docbookfile
+
+=head1 AUTHORS
+
+Daniel Brosseau <dab@catapulse.org>
+
+=head1 LICENSE
+
+This script is licensed under the same terms as Perl itself.
 
 =cut
