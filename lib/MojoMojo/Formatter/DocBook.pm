4b942faba5d92a7da969bf32e1fd16196bbe8633
dab () <dab@catapulse.org>
2009-04-05 13:14:43 +0200

    Move to_xhtml from File::DocBook to formatter

--- lib/MojoMojo/Formatter/DocBook.pm
+++ lib/MojoMojo/Formatter/DocBook.pm
@@ -51,7 +51,7 @@ sub format_content {
 
         if ($dbk) {
             if ( $line =~ m/^=docbook\s*$/ ) {
-                $$content .= MojoMojo::Formatter::File::DocBook->to_xhtml( $dbk );
+                $$content .= $class->to_xhtml( $dbk );
                 $dbk = "";
             }
             else { $dbk .= $line . "\n"; }
@@ -68,6 +68,93 @@ sub format_content {
 }
 
 
+=item to_xhtml <dbk>
+
+takes DocBook documentation and renders it as XHTML.
+
+=cut
+
+sub to_xhtml {
+    my ( $class, $dbk ) = @_;
+    my $result;
+
+
+    # Beurk
+    $dbk =~ s/&/_-_amp_-_;/g;
+
+    $dbk =~ s/^\s//;
+    # 1 - Mark lang 
+    # <programlisting lang="..."> to <programlisting lang="...">[lang=...] code [/lang]
+    my $my_Handler = MojoMojo::Formatter::DocBook::Colorize->new($debug);
+    $my_Handler->step('marklang');
+
+    my $parsersax = XML::SAX::ParserFactory->parser(
+                                                    Handler => $my_Handler,
+                                                );
+
+    my @markeddbk = eval{ $parsersax->parse_string($dbk)};
+    if ($@) {
+        return "\nDocument malformed : $@\n" ;
+    }
+    ;
+
+
+    # 2 - Transform with xslt
+    my $parser = XML::LibXML->new();
+    my $xslt   = XML::LibXSLT->new();
+
+    my $source = eval {$parser->parse_string("@markeddbk")};
+
+
+    if ($@) {
+        return "\nDocument malformed : line $@\n" ;
+    }
+    ;
+
+
+    my $style_doc = $parser->parse_file($xsltfile);
+    my $stylesheet = 
+      eval {
+          $xslt->parse_stylesheet($style_doc);
+      };
+
+#    warn "@_" if @_;
+
+
+    #return "XHTML XHTML XHTML";
+
+    # C'est ici que l'on peut ajouter le css, LANG ...
+    # voir http://docbook.sourceforge.net/release/xsl/current/doc/html/index.html
+    # et   http://www.sagehill.net/docbookxsl
+    my $results = $stylesheet->transform($source, XML::LibXSLT::xpath_to_string('section.autolabel' => '1', 'chapter.autolabel' => '1', 'suppress.navigation' => '1', 'generate.toc' => '0'));
+
+
+    my $format=0;
+
+    my $string=
+      eval{
+	$results->toString($format);
+      };
+
+    # 3 - Colorize Code [lang=...] ... code ... [/lang]
+    $my_Handler->step('colorize');
+
+    my @colorized=$parsersax->parse_string($string);
+
+    $string="@colorized";
+    $string =~ s/_-_amp_-_;/&/g;
+
+
+    # 4 - filter
+    # To adapt to mojomojo
+    # delete <?xml version ...>, <html>,</html>,<head>,</head>,<body>,</body>
+    $string =~ s/^.*<body>//s;
+    $string =~ s/<\/body>.*<\/html>//s;
+    $string =~ s/<a id=\"id\d*\"><\/a>//g;
+    $string =~ s/clear:\sboth//g;
+
+    return $string;
+}
 
 
 =back
