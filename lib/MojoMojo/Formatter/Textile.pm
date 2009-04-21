4eefb3aeeac9d09f640acd1b992f5e455ce561f8
Johannes Plunien <plu@pqpq.de>
2009-04-21 16:14:57 +0200

    Disabled Text::Textile encoding html entities at all until someone comes up with a better idea

--- lib/MojoMojo/Formatter/Textile.pm
+++ lib/MojoMojo/Formatter/Textile.pm
@@ -6,6 +6,13 @@ use Text::Textile;
 use Text::SmartyPants;
 my $textile = Text::Textile->new( flavor => "xhtml1", charset => 'utf-8' );
 
+# We do not want Text::Textile to encode HTML entities at all because it will encode something
+# like &gt; into &amp;gt; which sucks
+{
+    no strict ref;
+    *{"Text::Textile::encode_html"} = sub { my ($self, $html) = @_; return $html; };
+}
+
 =head1 NAME
 
 MojoMojo::Formatter::Textile - Texile formatting for your content
