3a6b0c6497361bf34ca7f5709035e7ddb04070d6
Dan Dascalescu <ddascaNOSPAMlescu@gmail.com>
2009-05-11 13:14:34 -0700

    Tidied up RSS.pm

--- lib/MojoMojo/Formatter/RSS.pm
+++ lib/MojoMojo/Formatter/RSS.pm
@@ -13,10 +13,10 @@ MojoMojo::Formatter::RSS - Include rss feeds on your page.
 
 =head1 DESCRIPTION
 
-This formatter takes a feed in the format {{feed://<url>}}, and 
+This formatter takes a feed in the format {{feed://<url>}}, and
 passes it through L<XML::Feed> to get a formatted feed suitable
-for inclusion in your page. It also caches them in the chosen 
-Catalyst Cache. By default it will render the first element in 
+for inclusion in your page. It also caches them in the chosen
+Catalyst Cache. By default it will render the first element in
 the feed, but it can take a numeric parameter to choose number of
 elements.
 
@@ -35,47 +35,47 @@ sub format_content_order { 4 }
 
 =item format_content
 
-calls the formatter. Takes a ref to the content as well as the
+Calls the formatter. Takes a ref to the content as well as the
 context object.
 
 =cut
 
 sub format_content {
-    my ($class,$content,$c)=@_;
+    my ($class, $content, $c) = @_;
     return unless $class->module_loaded;
-    my @lines=split /\n/,$$content;
+    my @lines=split /\n/, $$content;
     undef $$content;
     my $result;
     foreach my $line (@lines) {
-        if ($line =~ m/^\{\{(feed\:\/\/\S+)\s*(\d+)?\s*\}\}/) { 
-            $$content.=$class->include_rss($c,$1,$2);
+        if ($line =~ m/^\{\{(feed\:\/\/\S+)\s*(\d+)?\s*\}\}/) {
+            $$content .= $class->include_rss($c,$1,$2);
         } else {
-            $$content .=$line."\n";	
+            $$content .= $line."\n";
         }
-   }   
+   }
 }
 
 =item include_rss  <c> <url> [<entries>]
 
-returns html formatted content for inclusion.
+Returns HTML formatted content for inclusion.
 
 =cut
 
 sub include_rss {
-    my ($class,$c,$url,$entries)=@_;
+    my ($class, $c, $url, $entries)=@_;
     $entries ||= 1;
     $url =~ s/^feed/http/;
-    my $result=URI::Fetch->fetch($url,Cache=>$c->cache)->content;
-    my $feed=XML::Feed->parse(\$result) or
+    my $result = URI::Fetch->fetch($url,Cache=>$c->cache)->content;
+    my $feed = XML::Feed->parse(\$result) or
         return "Could not retrieve $url .\n";
-    my $count=0;
-    my $content='';
+    my $count = 0;
+    my $content = '';
     foreach my $entry ($feed->entries){
         $count++;
-        $content.='<div class="feed">'
-        .'<h3><a href="'.$entry->link.'">'.
-        ($entry->title||"no title").'</a></h3>'
-        .($entry->content->body||"")."</div>\n";
+        $content .= '<div class="feed">'
+          .'<h3><a href="'.$entry->link.'">'
+          .($entry->title||"no title").'</a></h3>'
+          .($entry->content->body||"")."</div>\n";
         return $content if $count==$entries;
     }
     return $content;
@@ -85,7 +85,7 @@ sub include_rss {
 
 =head1 SEE ALSO
 
-L<MojoMojo>,L<Module::Pluggable::Ordered>, L<XML::Feed>, L<URI::Fetch>
+L<MojoMojo>, L<Module::Pluggable::Ordered>, L<XML::Feed>, L<URI::Fetch>
 
 =head1 AUTHORS
 
