69052d06bdf0a95c6ca5cffd1795545c86eddc2d
mateu x hunter <hunter@missoula.org>
2009-05-12 20:58:39 -0400

    Turn off highlight on save and view edit.

--- lib/MojoMojo/Controller/Page.pm
+++ lib/MojoMojo/Controller/Page.pm
@@ -45,8 +45,10 @@ sub view : Global {
     $stash->{template} ||= 'page/view.tt';
 
     $c->forward('inline_tags');
-    $c->stash->{render} = 'highlight'
-      if $c->req->referer && $c->req->referer =~ /.edit$/;
+# NOTE: Highlight has been turned off until someone make is work perfectly in all cases.
+# In particular is sucks with TOC and valid HTML
+# $c->stash->{render} = 'highlight'
+#  if $c->req->referer && $c->req->referer =~ /.edit$/;
 
     my ( $path_pages, $proto_pages, $id ) =
       @$stash{qw/ path_pages proto_pages id /};
