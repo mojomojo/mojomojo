4773a72b24f9e29c309627b48df7df9e169e745a
Robert Litwiniec <linio@wonder.pl>
2009-05-14 11:32:08 +0200

    update_po.sh: javascript .po files renamed as $lang.js.po

--- script/update_po.sh
+++ script/update_po.sh
@@ -8,14 +8,15 @@ fi
 
 if [ -z "$1" ]
 then
-    langs=`ls lib/MojoMojo/I18N/|grep po$`
+    langs=`ls lib/MojoMojo/I18N/|grep ^..\.po$`
 else
     langs=$1.po
 fi
 
 for lang in $langs ; do
-    echo "lang: `echo $lang|sed 's/.po//'`"
-    perl -Ilib `which xgettext.pl` -now -D lib -D root/forms -D root/base -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang
-    perl -Ilib `which xgettext.pl` -now -D root/static -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang.json
-    perl -Ilib script/po2json.pl lib/MojoMojo/I18N/$lang.json root/static/json/$lang.json
+    lang_=`echo $lang|sed 's/.po//'`
+    echo "lang: $lang_"
+    perl -Ilib `which xgettext.pl` -now -D lib -D root/forms -D root/base -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang_.po
+    perl -Ilib `which xgettext.pl` -now -D root/static -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang_.js.po
+    perl -Ilib script/po2json.pl lib/MojoMojo/I18N/$lang_.js.po root/static/json/$lang_.po.json
 done
