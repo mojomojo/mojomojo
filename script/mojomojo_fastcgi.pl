05473b1b5b53b901efd98934ca684430da7fc6c8
mateu x hunter <hunter@missoula.org>
2009-07-29 13:38:06 -0400

    Refresh script/* to use restart from 1.18 ::Devel.

--- script/mojomojo_fastcgi.pl
+++ script/mojomojo_fastcgi.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 BEGIN { $ENV{CATALYST_ENGINE} ||= 'FastCGI' }
 
@@ -31,7 +31,7 @@ MojoMojo->run(
         pidfile => $pidfile,
         manager => $manager,
         detach  => $detach,
-	keep_stderr => $keep_stderr,
+        keep_stderr => $keep_stderr,
     }
 );
 
@@ -73,7 +73,7 @@ Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
