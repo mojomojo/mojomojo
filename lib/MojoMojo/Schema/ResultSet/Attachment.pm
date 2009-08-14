b65a1e54ec6aaaf80671e37e035f0de6afc47d80
Dan Dascalescu <ddascaNOSPAMlescu@gmail.com>
2009-08-14 09:35:31 +0000

    Commented out attempt to automatically unzip uploaded ZIP archives and corrected POD for Schema::ResultSet::Attachment

--- lib/MojoMojo/Schema/ResultSet/Attachment.pm
+++ lib/MojoMojo/Schema/ResultSet/Attachment.pm
@@ -20,36 +20,42 @@ MojoMojo::Schema::ResultSet::Attachment
 
 =head2 create_from_file (page, filename, storage_callback)
 
-Create an instance from a given file. Takes a filename, a page to attach to,
-and a storage callback. The storage callback will be called with a full path
-to where the file should be stored.
+Create an instance from a given file. Takes a page to attach to, the
+client-supplied filename, and the actual file.
 
 =cut
 
 sub create_from_file {
     my ( $class, $page, $filename, $file ) = @_;
     my $mm = File::MMagic->new();
-    if ( $mm->checktype_filename($filename) eq 'application/zip' ) {
-        my $zip;
-        $zip = Archive::Zip->new($file);
-        return unless $zip;
-        my @atts;
-        foreach my $member ( $zip->members ) {
-            next if $member->isDirectory;
-            my $tmpfile = tempfile;
-            $member->extractToFileNamed($tmpfile);
-            push @atts, $class->create_from_file( $page, $member->fileName, $tmpfile );
-        }
-        return @atts;
-    }
+    #if ( $mm->checktype_filename($filename) eq 'application/zip' ) {
+        # TODO: the file type returned for a ZIP is 'application/x-zip' (not 'application-zip'),
+        # so this has never actually worked. It also never worked because $filename is
+        # the client-supplied filename, not the actually uploaded file.
+        # Anyway, unpacking the ZIP willy-nilly is a silly idea.
+        # Commented out until a UI option to unpack uploaded ZIP(s) is added.
+        # --dandv
+        #my $zip;
+        #$zip = Archive::Zip->new($file);
+        #return unless $zip;
+        #my @atts;
+        #foreach my $member ( $zip->members ) {
+        #    next if $member->isDirectory;
+        #    my $tmpfile = tempfile;
+        #    $member->extractToFileNamed($tmpfile);
+        #    push @atts, $class->create_from_file( $page, $member->fileName, $tmpfile );
+        #}
+        #return @atts;
+    #}
 
-    my $self = $class->create(
-        {
-            name => $filename,
-            page => $page->id
-        }
-    );
-    die "Could not attach $filename to $page" unless $self;
+    my $self = $class->create({
+        name => $filename,
+        page => $page->id
+    });
+    die "Could not attach $filename to $page" if not $self;
+
+    # copy the passed $file (usually from the temporary upload directory), to the attachments directory,
+    # with the filename set to MojoMojo Schema::Result::Attachment->filename (currently the row id)
     File::Copy::copy( $file, $self->filename );
 
     my $fh = FileHandle->new( $self->filename . '' );
