2921ae5c800b23fc7abf255deaeb523767fad056
Robert Litwiniec <linio@wonder.pl>
2009-09-12 15:33:30 +0200

    script/update_po.sh: for js check only root/static/js directory

--- script/update_po.sh
+++ script/update_po.sh
@@ -17,6 +17,6 @@ for lang in $langs ; do
     lang_=`echo $lang|sed 's/.po//'`
     echo "lang: $lang_"
     perl -Ilib `which xgettext.pl` -now -D lib -D root/forms -D root/base -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang_.po
-    perl -Ilib `which xgettext.pl` -now -D root/static -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang_.js.po
+    perl -Ilib `which xgettext.pl` -now -D root/static/js -P perl=* -P tt2=* -P yaml=* -P formfu=* -P text=*  -o lib/MojoMojo/I18N/$lang_.js.po
     perl -Ilib script/po2json.pl lib/MojoMojo/I18N/$lang_.js.po root/static/json/$lang_.po.json
 done
