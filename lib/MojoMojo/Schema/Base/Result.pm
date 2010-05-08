2ba27adefe56a0750fa6296cab2a978501e2994a
Mateu X. Hunter <hunter@missoula.org>
2010-05-08 13:01:04 -0400

    Hook in MySQL table engine as InnoDB and CHARSET utf8

--- lib/MojoMojo/Schema/Base/Result.pm
+++ lib/MojoMojo/Schema/Base/Result.pm
@@ -4,6 +4,14 @@ use strict;
 use warnings;
 use parent qw/DBIx::Class/;
 
+sub sqlt_deploy_hook {
+    my ( $self, $sqlt_table ) = @_;
+    $sqlt_table->extra(
+        mysql_table_type => 'InnoDB',
+        mysql_charset    => 'utf8'
+    );
+}
+
 =head1 NAME
 
 MojoMojo::Schema::Base::Result - base class for Result classes
