52ffd6d25a74e0d0a6bca72b6907b6de0622769d
Marcus Ramberg <marcus@nordaaker.com>
2011-01-12 11:58:25 +0100

    Perltidy to fix notabs errors

--- lib/MojoMojo/Formatter/GoogleCalendar.pm
+++ lib/MojoMojo/Formatter/GoogleCalendar.pm
@@ -3,8 +3,6 @@ package MojoMojo::Formatter::GoogleCalendar;
 #use strict;
 use parent 'MojoMojo::Formatter';
 
-
-
 #my $dependencies_installed = !$@;
 #sub module_loaded { $dependencies_installed }
 
@@ -35,37 +33,37 @@ context object.
 
 =cut
 
-
 sub format_content {
     my ( $class, $content, $c ) = @_;
-	my ($width,$height,$align);
-	my $default_width = 600;
-	my $default_height = 400;
-	my $default_alignment = 'center';
-	my %alignment_style = ('center'=>'margin:auto;width:70%',
-			'right'=>'float:right;width:70%',
-			'left'=>'float:left;width:70%'
-		);
-	my @lines = split /\n/, $$content;
-	my $re = $class->gen_re(qr/gcal\s+(.*?)\s+(\d+),(\d+)\s+(\w+)/);
-	$$content = "";
-	$c->stash->{precompile_off} = 1;
+    my ( $width, $height, $align );
+    my $default_width     = 600;
+    my $default_height    = 400;
+    my $default_alignment = 'center';
+    my %alignment_style   = (
+        'center' => 'margin:auto;width:70%',
+        'right'  => 'float:right;width:70%',
+        'left'   => 'float:left;width:70%'
+    );
+    my @lines = split /\n/, $$content;
+    my $re = $class->gen_re(qr/gcal\s+(.*?)\s+(\d+),(\d+)\s+(\w+)/);
+    $$content = "";
+    $c->stash->{precompile_off} = 1;
 
-	foreach my $line (@lines) {
-		if ($line =~ m/$re/) {
-			!defined($2) ? $height=$default_height : $height=$2;	
-			!defined($3) ? $width=$default_width : $width=$3;	
-			!defined($4) ? $align=$alignment_style{$default_alignment} : $align=$alignment_style{$4};	
-			$line = "<div style='$align;border:1'><iframe src='$1' height='$height' width='$width' style='border-width:0; margin-left:auto; margin-right:auto' frameborder='0' scrolling='no'></iframe></div>";
-		}
-		$$content .= $line . "\n";
-	}
-	return $content;
-
+    foreach my $line (@lines) {
+        if ( $line =~ m/$re/ ) {
+            !defined($2) ? $height = $default_height : $height = $2;
+            !defined($3) ? $width  = $default_width  : $width  = $3;
+            !defined($4)
+              ? $align = $alignment_style{$default_alignment}
+              : $align = $alignment_style{$4};
+            $line =
+"<div style='$align;border:1'><iframe src='$1' height='$height' width='$width' style='border-width:0; margin-left:auto; margin-right:auto' frameborder='0' scrolling='no'></iframe></div>";
+        }
+        $$content .= $line . "\n";
+    }
+    return $content;
 
 }
-
-
 
 =head1 SEE ALSO
 
