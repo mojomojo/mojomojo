5fb918300399ccc628179dc3fc198dec2b68542e
Dan Dascalescu <ddascalescu+github@gmail.com>
2011-07-19 08:49:50 -0700

    Allow only logged-in users to export, to alleviate issue #39

--- lib/MojoMojo/Controller/Export.pm
+++ lib/MojoMojo/Controller/Export.pm
@@ -55,6 +55,11 @@ for all the nodes of the wiki.
 
 sub export_raw : Global {
     my ( $self, $c ) = @_;
+    if ( !$c->user_exists() ) {
+        $c->stash->{message} = $c->loc('To export, you must be logged in');
+        $c->detach('MojoMojo::Controller::PageAdmin', 'unauthorized');
+    }
+    
     my $prefix = generate_export_filename($c, 'markup');
 
     unless ( $c->res->{body} = $c->cache->get($prefix) ) {
@@ -85,6 +90,11 @@ versions of all the nodes of the wiki.
 
 sub export_html : Global {
     my ( $self, $c ) = @_;
+    if ( !$c->user_exists() ) {
+        $c->stash->{message} = $c->loc('To export, you must be logged in');
+        $c->detach('MojoMojo::Controller::PageAdmin', 'unauthorized');
+    }
+    
     my $prefix = generate_export_filename($c, 'html');
 
     unless ( $c->res->{body} = $c->cache->get($prefix) ) {
